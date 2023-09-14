a6a5a6f092258022c89d2f1165f6cc86186f6296
Mafa <144964725+Mahrus-Faris21@users.noreply.github.com>
2023-09-14 10:25:38 +0700

    Update Task Bucket FICO scores.py

--- Task Bucket FICO scores.py
+++ Task Bucket FICO scores.py
@@ -2,40 +2,28 @@
 # coding: utf-8
 
 # # Background Information About the Task
-
 # # Case 4: Display Information
-
 # Now that you are familiar with the portfolio and personal loans and risk are using your model as a guide to loss provisions for the upcoming year, the team now asks you to look at their mortgage book. They suspect that FICO scores will provide a good indication of how likely a customer is to default on their mortgage. Charlie wants to build a machine learning model that will predict the probability of default, but while you are discussing the methodology, she mentions that the architecture she is using requires categorical data. As FICO ratings can take integer values in a large range, they will need to be mapped into buckets. She asks if you can find the best way of doing this to allow her to analyze the data.
-# 
 # A FICO score is a standardized credit score created by the Fair Isaac Corporation (FICO) that quantifies the creditworthiness of a borrower to a value between 300 to 850, based on various factors. FICO scores are used in 90% of mortgage application decisions in the United States. The risk manager provides you with FICO scores for the borrowers in the bank’s portfolio and wants you to construct a technique for predicting the PD (probability of default) for the borrowers using these scores. 
 
 # # Case 4: Task Brief
-
 # Charlie wants to make her model work for future data sets, so she needs a general approach to generating the buckets. Given a set number of buckets corresponding to the number of input labels for the model, she would like to find out the boundaries that best summarize the data. You need to create a rating map that maps the FICO score of the borrowers to a rating where a lower rating signifies a better credit score.
-# 
-# The process of doing this is known as quantization. You could consider many ways of solving the problem by optimizing different properties of the resulting buckets, such as the mean squared error or log-likelihood (see below for definitions). For background on quantization, see here.
-# 
-# Mean squared error:
-# 
+# The process of doing this is known as quantization. You could consider many ways of solving the problem by optimizing different properties of the resulting buckets, such as the mean squared error or log-likelihood (see below for definitions). For background on quantization, see here. 
+# Mean squared error: 
 # You can view this question as an approximation problem and try to map all the entries in a bucket to one value, minimizing the associated squared error. We are now looking to minimize the following:
 # $$MSE = \frac{1}{n}\sum_{i=1}^{n}(y_i - \hat{y_i})^2$$
 # Log-likelihood:
-# 
 # A more sophisticated possibility is to maximize the following log-likelihood function.
 # $$\log L(\theta|y) = \sum_{i=1}^{n}\log f(y_i|\theta)$$
 # Where $b_i$ is the bucket boundaries, ni is the number of records in each bucket, $k_i$ is the number of defaults in each bucket, and $p_i = k_i / n_i$ is the probability of default in the bucket. This function considers how rough the discretization is and the density of defaults in each bucket. This problem could be addressed by splitting it into subproblems, which can be solved incrementally (i.e., through a dynamic programming approach). For example, you can break the problem into two subproblems, creating five buckets for FICO scores ranging from 0 to 600 and five buckets for FICO scores ranging from 600 to 850. Refer to this page for more context behind a likelihood function. This page may also be helpful for background on dynamic programming. 
 
 # In[1]:
-
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 # In[2]:
-
-
 csv_path = r'C:\Users\Sweet\Downloads\Portofolio\Task 3 and 4_Loan_Data.csv'
 data = pd.read_csv(csv_path)
 data.sort_index(inplace=True)
@@ -53,10 +41,7 @@ print(data.head())
 
 
 # # Determine The FICO Ratings
-
 # In[ ]:
-
-
 data.sort_index(inplace=True)
 
 # Define the number of intervals
@@ -106,8 +91,6 @@ print(data.head())
 # # Determine of Log-likelihood and Quantization Signal Processing
 
 # In[6]:
-
-
 def log_likelihood(x, mu, sigma):
     return np.sum(np.log(np.exp(-(x-mu)**2/(2*sigma**2))/(np.sqrt(2*np.pi)*sigma)))
 
@@ -159,8 +142,6 @@ def quantize_signal(x, k):
 
 
 # In[3]:
-
-
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import mean_squared_error
 
@@ -187,24 +168,16 @@ print('Log-likelihood:', log_likelihood)
 
 
 # To prepare the FICO scores for use in Charlie's machine learning model, we employ a technique known as quantization, which involves mapping the FICO scores into categorical buckets. The objective is to transform the continuous FICO scores into discrete categories to enable the model's analysis.
-# 
-# The optimization criterion for determining the bucket boundaries typically involves minimizing Mean Squared Error (MSE) or maximizing Log-likelihood. These criteria help identify the boundaries that best summarize the data while maintaining the model's predictive accuracy.
-# 
+# The optimization criterion for determining the bucket boundaries typically involves minimizing Mean Squared Error (MSE) or maximizing Log-likelihood. These criteria help identify the boundaries that best summarize the data while maintaining the model's predictive accuracy. 
 # For our specific dataset, the quantization process resulted in the following metrics:
-# 
 # Mean Squared Error (MSE): 0.1328
 # Log-likelihood: -4239.15
-# 
 # These metrics provide insights into the quality of the bucket boundaries chosen for mapping FICO scores. A lower MSE and a higher log-likelihood indicate a better fit of the bucket boundaries to the data.
-# 
 # By implementing this quantization technique, we can effectively prepare the FICO scores as categorical data for Charlie's machine learning model, allowing her to analyze and predict the probability of default for mortgage borrowers.
 # 
 
 # In[4]:
-
-
 fico_data = data['fico_score'].values
-
 mu = np.mean(fico_data)
 sigma = np.std(fico_data)
 p_range = np.linspace(mu-3*sigma, mu+3*sigma, 100)
