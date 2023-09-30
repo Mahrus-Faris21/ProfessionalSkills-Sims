9e798d593e71d4673d55853fbe576e810a20435e
Mafa <144964725+Mahrus-Faris21@users.noreply.github.com>
2023-09-30 10:54:53 +0700

    Update Feature Engineering & Modelling.py

--- Feature Engineering & Modelling.py
+++ Feature Engineering & Modelling.py
@@ -2,67 +2,46 @@
 # coding: utf-8
 
 # # Here is the background information on your task
-
 # The team now has a good understanding of the data and feels confident to use the data to further understand the business problem. The team now needs to brainstorm and build out features to uncover signals in the data that could inform the churn model.
-# 
-# Feature engineering is one of the keys to unlocking predictive insight through mathematical modeling. Based on the data that is available and was cleaned, identify what you think could be drivers of churn for our client and build those features to later use in your model.
-# 
+# Feature engineering is one of the keys to unlocking predictive insight through mathematical modeling. Based on the data that is available and was cleaned, identify what you think could be drivers of churn for our client and build those features to later use in your model. 
 # First focus on building on top of the feature that your colleague has already investigated: **“the difference between off-peak prices in December and January the preceding year”**. After this, if you have time, feel free to get creative with making any other features that you feel are worthwhile.
 # 
-# Once you have a set of features, you must train a Random Forest classifier to predict customer churn and evaluate the performance of the model with suitable evaluation metrics. Be rigorous with your approach and give full justification for any decisions made by yourself as the intern data scientist. 
-# 
-# Recall that the hypotheses under consideration is that churn is driven by the customers’ price sensitivities and that it would be possible to predict customers likely to churn using a predictive model.
-# 
+# Once you have a set of features, you must train a Random Forest classifier to predict customer churn and evaluate the performance of the model with suitable evaluation metrics. Be rigorous with your approach and give full justification for any decisions made by yourself as the intern data scientist.  
+# Recall that the hypotheses under consideration is that churn is driven by the customers’ price sensitivities and that it would be possible to predict customers likely to churn using a predictive model. 
 # If you’re eager to go the extra mile for the client, when you have a trained predictive model, remember to investigate the client’s proposed discounting strategy, with the head of the SME division suggesting that offering customers at high propensity to churn a 20% discount might be effective.
-# 
 # Build your models and test them while keeping in mind you would need data to prove/disprove the hypotheses, as well as to test the effect of a 20% discount on customers at high propensity to churn.
-# 
-# 
-
+ 
 # # Here is your task
-
 # **Sub-Task 1**
-# 
 # Your colleague has done some work on engineering the features within the cleaned dataset and has calculated a feature which seems to have predictive power. 
-# 
 # This feature is **“the difference between off-peak prices in December and January the preceding year”**. 
-# 
 # Run the cells in the notebook provided (named feature_engineering.ipynb) to re-create this feature. then try to think of ways to improve the feature’s predictive power and elaborate why you made those choices. 
-# 
 # You should spend 1 - 1.5 hours on this. Be sure to make use of the “feature_engineering.ipynb” notebook to get started with re-creating your colleagues' features.
-# 
+
 # **Sub-Task 2**
-# 
 # Now that you have a dataset of cleaned and engineered features, it is time to build a predictive model to see how well these features are able to predict a customer churning. It is your task to train a Random Forest classifier and to evaluate the results in an appropriate manner. We would also like you to document the advantages and disadvantages of using a Random Forest for this use case. It is up to you how to fulfill this task, but you may want to use the below points to guide your work:
-# 
 # Ensure you’re able to explain the performance of your model, where did the model underperform?
 # Why did you choose the evaluation metrics that you used? Please elaborate on your choices.
 # Document the advantages and disadvantages of using the Random Forest for this use case.
 # Do you think that the model performance is satisfactory? Give justification for your answer.
 # (Bonus) - Relate the model performance to the client's financial performance with the introduction of the discount proposition. How much money could a client save with the use of the model? What assumptions did you make to come to this conclusion?
 # You should spend 1 - 1.5 hours on this. When it comes to model evaluation and the explanation of your results, feel free to use the additional links below.
-# 
+
 # **If you are stuck:**
-# 
 # Sub-Task 1
-# 
 # - Think of ways to evaluate a feature against a label.
 # - Think of ways to add new features which would complement the already existing ones. 
 # - Think of feature granularity. 
 # - Remove unnecessary features.
-#  
+
 # Sub-Task 2
-# 
 # - Is this problem best represented as classification or regression? 
 # - What kind of model performance do you think is appropriate? 
 # - Most importantly how would you measure such a performance? 
 # - How would you tie business metrics such as profits or savings to the model performance?
 
 # # The Answers
-
 # In[76]:
-
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -78,10 +57,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import precision_score, recall_score, accuracy_score
 from sklearn.model_selection import StratifiedKFold
 
-
 # In[2]:
-
-
 file_path = r'C:\Users\Sweet\Downloads\Portofolio\client_data.csv'
 
 # Read the CSV file into a DataFrame
@@ -97,10 +73,7 @@ client_data["date_modif_prod"] = pd.to_datetime(client_data["date_modif_prod"], 
 client_data["date_renewal"] = pd.to_datetime(client_data["date_renewal"], format='%Y-%m-%d')
 client_data.head()
 
-
 # In[3]:
-
-
 file_path = r'C:\Users\Sweet\Downloads\Portofolio\price_data.csv'
 
 # Read the CSV file into a DataFrame
@@ -113,10 +86,7 @@ price_data.reset_index(drop=True, inplace=True)
 price_data["price_date"] = pd.to_datetime(price_data["price_date"], format='%Y-%m-%d')
 price_data.head()
 
-
 # In[4]:
-
-
 # Group off-peak prices by companies and month
 monthly_price_by_id = price_data.groupby(['id', 'price_date']).agg({'price_off_peak_var': 'mean', 'price_off_peak_fix': 'mean'}).reset_index()
 
@@ -131,10 +101,7 @@ diff['offpeak_diff_dec_january_power'] = diff['dec_2'] - diff['price_off_peak_fi
 diff = diff[['id', 'offpeak_diff_dec_january_energy','offpeak_diff_dec_january_power']]
 diff.head()
 
-
 # In[24]:
-
-
 numeric_columns = X.select_dtypes(include=['number']).columns.tolist()
 
 # Handle missing values for numeric columns (impute with the mean)
@@ -166,8 +133,6 @@ print(f'ROC AUC Score: {roc_auc}')
 
 
 # In[44]:
-
-
 stat_ = ['max', 'min', 'mean']
 price_attr = ['price_off_peak_var', 'price_peak_var', 'price_mid_peak_var', 'price_off_peak_fix', 'price_peak_fix', 'price_mid_peak_fix']
 
@@ -184,8 +149,6 @@ price_stat.head(5)
 
 
 # In[48]:
-
-
 price_stat = (
     price_stat
     .merge(client_data[['id', 'churn']], on=['id'], how='left')
@@ -195,10 +158,7 @@ price_stat = (
 
 price_stat.head(5)
 
-
 # In[49]:
-
-
 price_stat = (
     price_stat
     .assign(**{f'diff_max_min_{attr}': lambda x: x[f'{attr}_max'] - x[f'{attr}_min'] for attr in price_attr})
@@ -206,19 +166,13 @@ price_stat = (
 
 price_stat.head(7)
 
-
 # In[53]:
-
-
 for attr in price_attr:
     price_stat[f'diff_Dec_mean_{attr}'] = price_data[price_data['id'].isin(price_stat['id'])].groupby(['id'])[attr].nth(-1).values - price_stat[f'{attr}_mean']
 
 price_stat.head(7)
 
-
 # In[71]:
-
-
 # Filter the price DataFrame for dates after '2015-06-01'
 price_filtered = price_data[price_data['price_date'] > '2015-06-01']
 
@@ -245,10 +199,7 @@ for attr in ['price_off_peak_var', 'price_off_peak_fix']:
 # Display the first 7 rows of the resulting dataframe
 price_stat_6_month.head(7)
 
-
 # In[72]:
-
-
 # Filter the price DataFrame for dates after '2015-09-01'
 price_filtered = price_data[price_data['price_date'] > '2015-09-01']
 
@@ -275,10 +226,7 @@ for attr in ['price_off_peak_var', 'price_off_peak_fix']:
 # Display the first 7 rows of the resulting dataframe
 price_stat_3_month.head(7)
 
-
 # In[74]:
-
-
 # Prepare the data and drop unnecessary columns
 train_data = client_data.copy()
 train_data['year_modif_prod'] = train_data['date_modif_prod'].dt.year
@@ -326,10 +274,7 @@ train_data = train_data.merge(price_stat[['id'] + price_stat_attributes], on='id
 # Display the first few rows of the resulting DataFrame
 train_data.head()
 
-
 # In[75]:
-
-
 # Drop columns 'id' and 'churn' to create the feature matrix X and target vector y
 X = train_data.drop(columns=['id', 'churn'])
 y = train_data['churn']
@@ -337,10 +282,7 @@ y = train_data['churn']
 # Check the shape of X and y
 X.shape, y.shape
 
-
 # In[77]:
-
-
 def train_and_evaluate_rf(X, y, random_state=56, n_splits=5, shuffle=True):
     # Create an array to store predicted training labels
     pred_train_labels = np.zeros(shape=(X.shape[0], 2))
@@ -392,10 +334,7 @@ feature_importance_df, total_precision, total_recall, total_accuracy = train_and
 # Print overall metrics
 print(f"Total Precision {total_precision:.3f} Recall {total_recall:.3f} Accuracy {total_accuracy:.3f}")
 
-
 # In[80]:
-
-
 def plot_feature_importance(feature_importance_df, figsize=(12, 10), palette='winter'):
     # Create a figure and axis
     fig, ax = plt.subplots(figsize=figsize)
@@ -418,10 +357,7 @@ def plot_feature_importance(feature_importance_df, figsize=(12, 10), palette='wi
 # Call the function to plot feature importances with a custom color palette
 plot_feature_importance(feature_importance_df, palette='winter')
 
-
 # In[92]:
-
-
 # Predicted labels
 pred_train_labels = np.zeros(shape=(X.shape[0], 2))
 pred_y = pred_train_labels.argmax(axis=-1)
@@ -434,10 +370,7 @@ total_accuracy = accuracy_score(y, pred_y)
 # Print classification report
 print(classification_report(y, pred_y))
 
-
 # In[91]:
-
-
 # Calculate the confusion matrix
 CM = confusion_matrix(y, pred_y)
 
@@ -453,10 +386,7 @@ plt.ylabel('True Labels')
 plt.title('Confusion Matrix')
 plt.show()
 
-
 # In[103]:
-
-
 def engineer_features(data, price_data, discount=0.8):
     # Drop unnecessary columns
     data = data.drop(columns=['date_activ', 'date_end', 'date_modif_prod', 'date_renewal'])
@@ -479,10 +409,7 @@ test_data = engineer_features(client_data.copy(), price_stat_3_month)
 # Display the resulting test_data
 test_data.head()
 
-
 # In[107]:
-
-
 # Initialize arrays to store results for each fold
 precisions, recalls, accuracies = [], [], []
 prob_no_discount_list, prob_discount_list = [], []
@@ -517,14 +444,10 @@ for fold_counter, (train_index, test_index) in enumerate(kfold.split(X, y), star
     prob_test = rf.predict_proba(X_test)
     prob_discount_list.append(prob_test)
 
-
 # In[108]:
-
-
 # Calculate and print the mean values across folds
 mean_precision = sum(precisions) / len(precisions)
 mean_recall = sum(recalls) / len(recalls)
 mean_accuracy = sum(accuracies) / len(accuracies)
 
 print(f"Mean Precision {mean_precision:.3f} Mean Recall {mean_recall:.3f} Mean Accuracy {mean_accuracy:.3f}")
-
